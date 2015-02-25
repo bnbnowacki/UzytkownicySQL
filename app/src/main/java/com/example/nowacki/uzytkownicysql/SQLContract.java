2967aadafb29b2b6478f1b5ad42079cf3e743afa
bnbnowacki <bnbnowacki@gmail.com>
2015-02-26 00:54:00 +0100

    Initial commit

---
+++ app/src/main/java/com/example/nowacki/uzytkownicysql/SQLContract.java
@@ -0,0 +1,17 @@
+package com.example.nowacki.uzytkownicysql;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Nowacki on 2015-02-26.
+ */
+public class SQLContract {
+    public SQLContract() {}
+
+    public static abstract class SQLDane implements BaseColumns{
+        public static final String TABLE_NAME = "uzytkownicy";
+        public static final String COLUMN_NAME_ID="_id";
+        public static final String COLUMN_NAME_NAZWA="nazwa";
+        public static final String COLUMN_NAME_HASLO="haslo";
+    }
+}
